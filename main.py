a6afb11335e9f23960b9c1ba444b92a77b856d27
Christian Brinkley <christian.a.brinkley@gmail.com>
2018-04-12 19:49:37 -0500

    Fixed formating and Added date time and ordering by most recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -13,11 +14,19 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(10000))
+    date = db.Column(db.DateTime)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, date = None):
         self.title = title
         self.body = body
+        if date is None:
+            date = datetime.utcnow()
+        self.date = date
 
+#temp redirect
+@app.route('/')
+def main():
+    return redirect('/blog')
 
 @app.route('/blog', methods=['GET'])
 def index():
@@ -27,20 +36,9 @@ def index():
         blog = Blog.query.get(blog_id)
         return render_template('post.html', title="Post", blog=blog)
 
-        #task_name = request.form['task']
-        #new_task = Task(task_name)
-        #db.session.add(new_task)
-        #db.session.commit()
-
     else:
-        blogs = Blog.query.all()
+        blogs = Blog.query.order_by(Blog.date.desc()).all()
         return render_template('main.html', title='Main Page', blogs = blogs)
-
-    #tasks = Task.query.filter_by(completed=False).all()
-    #completed_tasks = Task.query.filter_by(completed=True).all()
-    #return render_template('todos.html',title="Get It Done!", 
-        #tasks=tasks, completed_tasks=completed_tasks)
-
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def delete_task():
@@ -50,25 +48,19 @@ def delete_task():
         blog_body = request.form['body']
         blog_title_error = False
         blog_body_error = False
-        if not blog_title or not blog_body:
+        if not blog_title or not blog_body:  
             if not blog_title:
                 blog_title_error = True
             if not blog_body:
                 blog_body_error = True
-            return render_template('newpost.html', title = 'Add a Blog Entry', blog_title = blog_title, blog_body = blog_body, blog_title_error = blog_title_error, blog_body_error = blog_body_error)
+            return render_template('newpost.html',  title = 'Add a Blog Entry', blog_title = blog_title, blog_body = blog_body, blog_title_error = blog_title_error, blog_body_error = blog_body_error)
         else:
             new_blog = Blog(blog_title, blog_body)
             db.session.add(new_blog)
             db.session.commit()
             return redirect('/blog')
 
-    #task = Task.query.get(task_id)
-    #task.completed = True
-    #db.session.add(task)
-    #db.session.commit()
-
     return render_template('newpost.html', title = 'Add a Blog Entry')
-
 
 if __name__ == '__main__':
     app.run()
